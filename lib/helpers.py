acaa8555a9ace55cb9e1b4de0ef71ec167c6cea2
Timothy Ryon <tim@timryon.com>
2024-04-19 20:44:23 -0400

    finish codealong

--- lib/helpers.py
+++ lib/helpers.py
@@ -10,27 +10,56 @@ def exit_program():
 
 
 def list_departments():
-    pass
-
+    departments = Department.get_all()
+    for department in departments:
+        print(department)
 
 def find_department_by_name():
-    pass
-
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    print(department) if department else print(
+        f"Department {name} not found")
 
 def find_department_by_id():
-    pass
-
+    # use a trailing underscore to not override the built-in id function
+    id_ = input("Enter the department's id: ")
+    department = Department.find_by_id(id_)
+    print(department) if department else print(f'Department {id_} not found')
 
 def create_department():
-    pass
+    name = input("Enter the department's name: ")
+    location = input("Enter the department's location: ")
+    try:
+        department = Department.create(name, location)
+        print(f'Success: {department}')
+    except Exception as exc:
+        print("Error creating department: ", exc)
 
 
 def update_department():
-    pass
+    id_ = input("Enter the department's id: ")
+    # the := is known as the 'walrus' operator, an assignment expression introduced in Py3.8.  It assigns the expression on the right to the variable on the left while also returning the value.  In this case, it also evaluates if the expression is Truthy (ie not None) If a dept with the id is found, it evaluates to True and the block inside the if statement is run
+    if department := Department.find_by_id(id_):
+        try:
+            name = input("Enter the department's new name: ")
+            department.name = name
+            location = input("Enter the department's new location: ")
+            department.location = location
 
+            department.update()
+            print(f'Success: {department}')
+        except Exception as exc:
+            print("Error updating department: ", exc)
+        else:
+            print(f'Department {id_} not found')
 
 def delete_department():
-    pass
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        department.delete()
+        print(f'Department {id_} deleted')
+    else:
+        print(f'Department {id_} not found')
 
 
 # You'll implement the employee functions in the lab
